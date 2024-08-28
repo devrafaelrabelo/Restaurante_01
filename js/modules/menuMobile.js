25eef25da5c2d507e22fcc40f2c15c91cc0dbf22
Dev | Rafael Rabelo <117826764+devrafaelrabelo@users.noreply.github.com>
2024-08-27 23:16:04 -0300

    feat: configurando html/css menuMobile

---
+++ js/modules/menuMobile.js
@@ -0,0 +1,27 @@
+export default class MenuMobile {
+  constructor() {
+    this.btnMenu = document.querySelector('[data-menu="button"]')
+    this.menuList = document.querySelector('[data-menu="list"]')
+
+    this.eventToggleMenuMobile = this.eventToggleMenuMobile.bind(this)
+  }
+
+  toggleMenuMobile() {
+    this.menuList.classList.toggle('ativo')
+    this.btnMenu.classList.toggle('ativo')
+  }
+
+  eventToggleMenuMobile(e) {
+    this.toggleMenuMobile()
+  }
+
+  addEvents() {
+    ['touchstart', 'click'].forEach((evento) => {
+      this.btnMenu.addEventListener(evento, this.eventToggleMenuMobile)
+    })
+  }
+
+  init() {
+    this.addEvents();
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,4 @@
+import MenuMobile from './modules/menuMobile.js'
+
+const menuMobile = new MenuMobile();
+menuMobile.init();
